b8c459116e1e5d49a5cef59043ca091f258df0de
scolorado <224scolorado@frshd.com>
2023-12-07 09:12:32 -0500

    added fmore code

--- app.js
+++ app.js
@@ -30,12 +30,21 @@
 //     console.log(book.textContent)
 // }
 
-const books = document.querySelectorAll('#book-list li .name');
+// const books = document.querySelectorAll('#book-list li .name');
 
-Array.from(books).forEach(function(book){
-  book.textContent += ' (Book title)';
-});
+// Array.from(books).forEach(function(book){
+//   book.textContent += ' (Book title)';
+// });
 
-const bookList = document.querySelector('#book-list');
-bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML content</p>';
+// const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+// bookList.innerHTML += '<p>This is how you add HTML content</p>';
+
+const banner = document.querySelector('#page-banner');
+
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
