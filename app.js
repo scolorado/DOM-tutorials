20c3bb59b806c0ac49ebed56b33faa38ec4be736
scolorado <224scolorado@frshd.com>
2023-12-07 08:47:34 -0500

    eduted ut

---
+++ app.js
@@ -0,0 +1,26 @@
+// const search = document.getElementById('search-books');
+// const bookList = document.getElementById('book-list');
+
+// console.log(search, bookList);
+
+// const titles = document.getElementsByClassName('title');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(title){
+//   console.log(title);
+// });
+
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
+});
