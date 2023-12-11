ae1fd7f532e17bb305e5d293ba6f2c5c4e9b0b1f
scolorado <224scolorado@frshd.com>
2023-12-11 11:46:33 -0500

    finished vid 18

--- app.js
+++ app.js
@@ -345,3 +345,21 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     });
   });
+
+  // tabbed content
+  const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', (e) => {
+    if(e.target.tagName == 'LI'){
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      Array.from(panels).forEach((panel) => {
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
+        }
+      });
+    }
+  });
+
+})
